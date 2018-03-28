01d5df21a9afec435d7532adbc911cf18366456a
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-03-28 08:03:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // write your code below!
-()=>{}
+function happyHollidays(){
+  return 'Happy holidays!'
+}
+
+function happyHolidaysTo(person){
+  return `Happy holidays, ${person}`
+}
+
+function happyHoliday(day, person){
+  `Happy ${day}, ${person}`
+}
+
+function holidayCountdown(day, remain)
